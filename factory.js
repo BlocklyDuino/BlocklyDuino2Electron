7e6f7587a2aa3f53e42b9e7fdaeb66ed1b3b0527
SebCanet <canet.s@free.fr>
2020-04-02 16:42:27 +0200

    update SerialMonitor

--- factory.js
+++ factory.js
@@ -1,28 +1,29 @@
-var remote = require('electron').remote 
-var { ipcRenderer } = require("electron")
-var fs = require('fs')
+﻿var remote = require('electron').remote;
+var {ipcRenderer} = require("electron");
+var fs = require('fs');
 
 window.addEventListener('load', function load(event) {
-	var window = remote.getCurrentWindow()
-	if(!window.isMaximized())window.maximize()
-	document.getElementById('btn_quit').onclick = function() {
-		window.close()
-	}
-	document.getElementById('btn_max').onclick = function() {
-		if(window.isMaximized()){
-            window.unmaximize()
-			document.getElementById('btn_max').innerHTML="<span class='fa fa-window-maximize fa-lg'></span>"
-        }else{
-            window.maximize()
-			document.getElementById('btn_max').innerHTML="<span class='fa fa-window-restore fa-lg'></span>"
+    var window = remote.getCurrentWindow();
+    if (!window.isMaximized())
+        window.maximize();
+    document.getElementById('btn_quit').onclick = function () {
+        window.close();
+    };
+    document.getElementById('btn_max').onclick = function () {
+        if (window.isMaximized()) {
+            window.unmaximize();
+            document.getElementById('btn_max').innerHTML = "<span class='fa fa-window-maximize fa-lg'></span>";
+        } else {
+            window.maximize();
+            document.getElementById('btn_max').innerHTML = "<span class='fa fa-window-restore fa-lg'></span>";
         }
-	}
-	document.getElementById('btn_add').onclick = function(){
-		var dataB = document.getElementById('languagePre').textContent
-		var dataG = editor.getValue()
-		fs.writeFileSync('./www/blocs&generateurs/factory/add.js', dataB + "\n" + dataG)
-		localStorage.setItem("factoryBlock",blockType)
-		ipcRenderer.send("addBlock", blockType)
-		window.close()
-	}
-})
+    };
+    document.getElementById('btn_add').onclick = function () {
+        var dataB = document.getElementById('languagePre').textContent;
+        var dataG = editor.getValue();
+        fs.writeFileSync('./www/blocs&generateurs/factory/add.js', dataB + "\n" + dataG);
+        localStorage.setItem("factoryBlock", blockType);
+        ipcRenderer.send("addBlock", blockType);
+        window.close();
+    };
+});
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// Modules to control application life and create native browser window
-const {
-    app,
-    BrowserWindow,
-    electron,
-    ipcMain,
-    globalShortcut,
-	Tray
-} = require('electron');
+﻿// Modules to control application life and create native browser window
+        const {
+            app,
+            BrowserWindow,
+            electron,
+            ipcMain,
+            globalShortcut,
+            Tray
+        } = require('electron');
 
 let BlocklyWindow = null;
 let SerialWindow = null;
@@ -16,81 +16,93 @@ let tray = null;
 
 function createBlocklyWindow() {
     BlocklyWindow = new BrowserWindow({
-            width: 1510,
-            height: 700,
-            webPreferences: {
-                nodeIntegration: true
-            },
-			icon: __dirname + '/src/icon.ico'
-        });
+        width: 1510,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: __dirname + '/src/icon.ico'
+    });
     if (process.platform == 'win32' && process.argv.length >= 2) {
         BlocklyWindow.loadFile('./www/index.html?url=' + process.argv[1]);
     } else {
         BlocklyWindow.loadFile('./www/index.html');
-    };
+    }
+    ;
     BlocklyWindow.loadFile('./www/index.html');
     BlocklyWindow.setMenu(null);
     BlocklyWindow.on('closed', function () {
         BlocklyWindow = null;
     });
-};
+}
+;
 
 function createSerialWindow() {
     SerialWindow = new BrowserWindow({
-            width: 640,
-            height: 530,
-            'parent': BlocklyWindow,
-            resizable: false,
-            movable: true,
-            frame: true,
-            modal: false,
-			icon: __dirname + './src/icon.ico'
-        });
+        width: 640,
+        height: 530,
+        'parent': BlocklyWindow,
+        resizable: false,
+        movable: true,
+        frame: true,
+        modal: false,
+        icon: __dirname + './src/icon.ico'
+    });
     SerialWindow.loadFile('./www/blocklyduino/serialMonitor.html');
     SerialWindow.setMenu(null);
     SerialWindow.on('closed', function () {
         SerialWindow = null;
     });
-};
+    devtools = new BrowserWindow();
+    SerialWindow.webContents.setDevToolsWebContents(devtools.webContents);
+    SerialWindow.webContents.openDevTools({
+        mode: 'detach'
+    });
+}
+;
 
 function createFactoryWindow() {
     FactoryWindow = new BrowserWindow({
-            width: 1066,
-            height: 640,
-            'parent': BlocklyWindow,
-            resizable: true,
-            movable: true,
-            frame: false,
-            modal: false
-        });
+        width: 1066,
+        height: 640,
+        'parent': BlocklyWindow,
+        resizable: true,
+        movable: true,
+        frame: false,
+        modal: false
+    });
     FactoryWindow.loadFile('./www/blocksfactory/blocksfactory.html');
     FactoryWindow.setMenu(null);
     FactoryWindow.on('closed', function () {
         FactoryWindow = null;
     });
-};
+}
+;
 
 function openDevTools(BlocklyWindow = BrowserWindow.getFocusedWindow()) {
     if (BlocklyWindow) {
         BlocklyWindow.webContents.toggleDevTools();
-    };
-};
+    }
+    ;
+}
+;
 
 function refresh(BlocklyWindow = BrowserWindow.getFocusedWindow()) {
     BlocklyWindow.webContents.reloadIgnoringCache();
-};
+}
+;
 
 app.on('ready', () => {
     createBlocklyWindow();
     globalShortcut.register('F12', openDevTools);
     globalShortcut.register('F5', refresh);
-    devtools = new BrowserWindow();
-    BlocklyWindow.webContents.setDevToolsWebContents(devtools.webContents);
-    BlocklyWindow.webContents.openDevTools({
-       mode: 'detach'
-    });
-	tray = new Tray('./www/blocklyduino/media/logo_only.png');
-	tray.setToolTip('BlocklyDuino')
+    // devtools = new BrowserWindow();
+    // BlocklyWindow.webContents.setDevToolsWebContents(devtools.webContents);
+    // BlocklyWindow.webContents.openDevTools({
+        // mode: 'detach'
+    // });
+    tray = new Tray('./www/blocklyduino/media/logo_only.png');
+    tray.setToolTip('BlocklyDuino');
 });
 
 app.on('activate', function () {
